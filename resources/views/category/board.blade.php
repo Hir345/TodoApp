37c170013a25dc3c2d33385589f187b0f8343ff9
Hir345 <nakarikeri@gmail.com>
2022-03-05 17:11:35 +0900

    made deadline column

--- resources/views/category/board.blade.php
+++ resources/views/category/board.blade.php
@@ -14,7 +14,8 @@
         <table class="table">
             @foreach($items as $item)
                 <tr>
-                    <th><a href="/board/{{$item->getId()}}">{{$item->getData()}}</a></th>
+                    <th><a href="/board/{{$item->getId()}}">{{$item->getName()}}</a></th>
+                    <td>{{$item->getDl()}}まで</td>
                     <td><a href="/board/{{$item->getId()}}/edit">編集</a></td>
                     <td><a href="/board/{{$item->getId()}}/del">削除</a></td>
                 </tr>
@@ -25,13 +26,6 @@
 
         <!-- Option 1: Bootstrap Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        <!-- Option 2: Separate Popper and Bootstrap JS -->
-        <!--
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-        -->
-
-
     </body>
     <footer>
         {{$user->name}}としてログイン中
